f597417944c4b6e82fa1c03e5d1eb70e42979a69
AngelDhakal <anjaldhakal01@gmail.com>
2021-05-07 19:04:31 +0545

    getting total commit & rollback for db from pg_stat_database table & adding them to get qps

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -22,5 +24,20 @@ func main() {
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
-	_ = db
+	var qps int
+
+	// qps (Query Per Second) is the combination of total commits and rollbacks
+	// in a database so to get qps for a specific database in postgresql first get the
+	// total commits for a database from pg_stat_database table which stores all the
+	// transactions, commits, rollbacks for each database, and get the total rollback
+	// for specific database from the same pg_stat_database table and add them together
+	// then qps is acquired
+	row := db.QueryRow("SELECT xact_commit+xact_rollback FROM pg_stat_database WHERE datname = $1", dbName)
+
+	// retreiving the qps value from the row
+	err = row.Scan(&qps)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	log.Println(qps)
 }
